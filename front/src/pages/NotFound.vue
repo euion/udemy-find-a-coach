f463d2af0ede6b6ad5066aa43d78b24ef473f4be
euion <qq212420@gmail.com>
2023-08-11 17:24:00 +0900

    Feat 접근불가안내페이지추가

--- front/src/pages/NotFound.vue
+++ front/src/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>test</div>
+  <section>
+    <base-card>
+      <h2>페이지를 찾을 수 없습니다.</h2>
+      <p>
+        페이지를 찾을 수 없습니다.
+        <router-link to="/coaches">코치명단</router-link>을 확인해주세요.
+      </p>
+    </base-card>
+  </section>
 </template>
 
 <script>
